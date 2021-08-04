c158c1db27916f1c08846f7136160685eac376e4
tk-dev0927 <thkim009@gmail.com>
2021-08-05 05:09:52 +1000

    remove db file

--- ExtendAuth/settings.py
+++ ExtendAuth/settings.py
@@ -19,6 +19,7 @@ from django.core.exceptions import ImproperlyConfigured
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
